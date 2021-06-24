ffd427c20e20702423d553c0d54c5183b6640b76
Shawn O'Neil <oneilsh@gmail.com>
2021-06-23 22:40:45 -0700

    refresh

--- public/MusicWheel/makebundles.sh
+++
@@ -1,6 +0,0 @@
-rm *.zip
-zip -r MusicWheel_OSX.zip application.macosx
-zip -r MusicWheel_linux32.zip application.linux32
-zip -r MusicWheel_linux64.zip application.linux64
-zip -r MusicWheel_windows32.zip application.windows32
-zip -r MusicWheel_windows64.zip application.windows64
